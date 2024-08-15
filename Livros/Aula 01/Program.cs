4d21f84f2bd3ec36541cbf3428e6c854397981c8
Gilmar Antes Junior <427669@unoesc.lan>
2024-08-14 22:07:38 -0300

    Primeiro Commit

---
+++ Livros/Aula 01/Program.cs
@@ -0,0 +1,18 @@
+﻿// comentario de uma linha
+
+/* comentario de 
+multiplas linhas
+
+*/
+
+
+// Declarando variavel
+
+using System.Threading.Tasks.Dataflow;
+
+int number;
+// Atribuindo valor
+number = 10;
+
+Writeline number
+

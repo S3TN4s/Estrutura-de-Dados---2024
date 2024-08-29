84b10f028767bd0c752edcd6fe67bc5fbe255aca
Gilmar Antes Junior <427669@unoesc.lan>
2024-08-28 21:34:27 -0300

    Aula 28-08

---
+++ Atividades/Aula02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Metadata.Ecma335;
+using Aula02;
+
+Person person = new Person();
+Console.WriteLine(person.Name);
+
+Person person1
+    = new Person("Gilmar Junior", 19);
+Console.WriteLine(person1.Name);
+
+Person person2 = new Person();
+person2.Name = "Renato Garcia";
+person2.Age = 32;
+Console.WriteLine(person2.Name);
+
+Person person3 = new Person()
+{
+     Name = "Bolsonaro",
+     Age = 70
+
+};
+
+Console.WriteLine(person3.Name);

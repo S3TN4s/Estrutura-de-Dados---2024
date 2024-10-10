3fcb7fb2769cb16cd14c8143c80694d43f57ff50
Gilmar Antes Junior <427669@unoesc.lan>
2024-10-09 21:54:53 -0300

    InsertionSort

---
+++ Atividades/AlgoritimosOrdenacao/bubblesort/Program.cs
@@ -0,0 +1,14 @@
+﻿using bubblesort;
+
+int[] inteiros = {0, 12, 7, 1, 2, 8};
+
+bubblesort.BubbleSort.Print(inteiros);
+
+bubblesort.BubbleSort.Sort<int>(inteiros);
+
+bubblesort.BubbleSort.Print(inteiros);
+
+char[ ] chars = ['x', 'z', 'b', 'c', 'a'];
+bubblesort.BubbleSort.Print(chars);
+bubblesort.BubbleSort.Sort(inteiros);
+bubblesort.BubbleSort.Print(inteiros);
---
+++ Atividades/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using InsertionSort;
+int[] numbers = { 0, 3, 2, 1, 5, 4};
+
+InsertionSort.InsertionSort.Print(numbers);
+InsertionSort.InsertionSort.Sort<int>(numbers);
+InsertionSort.InsertionSort.Print(numbers);
